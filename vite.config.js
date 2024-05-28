00f648b86dbfc2e3ee33f67823f511a53372fee7
Vivek-122005 <151515473+Vivek-122005@users.noreply.github.com>
2024-05-28 14:48:53 +0530

    finish

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/EndSem-The-Soul-Store-Clone---Endsem-Project-The-Souled-Store---uutf0465e3vq/",
 })
